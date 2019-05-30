0851a10b62b796f93293c144d1b64bf7352d4dfd
Andre de Fontana Ignacio <ignacio83@gmail.com>
2019-05-30 08:23:15 -0300

    Demo

--- src/main/java/com/demo/webflux/controller/EmployeeController.java
+++ src/main/java/com/demo/webflux/controller/EmployeeController.java
@@ -28,49 +28,37 @@ class EmployeeController {
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public Mono<EmployeeContract> create(@RequestBody @Valid Mono<CreateEmployeeContract> employee) {
-    return employee
-        .map(CreateEmployeeContract::toDomain)
-        .publish(service::save)
-        .map(EmployeeContract::new);
+    // TODO
+    return Mono.empty();
   }
 
   @GetMapping
   public Flux<EmployeeContract> list() {
-    return service.findAll().map(EmployeeContract::new);
-  }
-
-  @GetMapping("/{id}/directs")
-  public Flux<EmployeeContract> directs(@PathVariable("id") Integer id) {
-    return service.findDirectsByBoss(id).map(EmployeeContract::new);
-  }
-
-  @GetMapping("/{id}/directs/count")
-  public Mono<Long> directsCount(@PathVariable("id") Integer id) {
-    return service.findDirectsByBoss(id).count();
+    // TODO
+    return Flux.empty();
   }
 
   @GetMapping("/{id}/all")
   public Flux<EmployeeContract> findAllByBoss(@PathVariable("id") Integer id) {
-    return service.findAllByBoss(id).map(EmployeeContract::new);
-  }
-
-  @GetMapping("/{id}/all/count")
-  public Mono<Long> countAllByBoss(@PathVariable("id") Integer id) {
-    return service.findAllByBoss(id).count();
+    // TODO
+    return Flux.empty();
   }
 
   @GetMapping("/{id}/all/cost")
-  public Mono<Integer> listAllByIdx(@PathVariable("id") Integer id) {
-    return service.costByBoss(id);
-  }
-
-  @GetMapping("/statistics")
-  public Mono<StatisticsContract> statistics() {
-    return service.statistics().map(tuple -> new StatisticsContract(tuple.getT1(), tuple.getT2()));
+  public Mono<Integer> costByBoss(@PathVariable("id") Integer id) {
+    // TODO
+    return Mono.empty();
   }
 
   @PostMapping("/export")
   public Flux<ExportContract> export() {
-    return service.export().map(ExportContract::new);
+    // TODO
+    return Flux.empty();
+  }
+
+  @GetMapping("/statistics")
+  public Mono<StatisticsContract> statistics() {
+    // TODO
+    return Mono.empty();
   }
 }
--- src/main/java/com/demo/webflux/service/EmployeeService.java
+++ src/main/java/com/demo/webflux/service/EmployeeService.java
@@ -8,13 +8,6 @@ import org.springframework.stereotype.Service;
 import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
-import reactor.util.function.Tuple2;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import java.time.Duration;
-import java.util.Comparator;
 
 @RequiredArgsConstructor
 @Service
@@ -23,88 +16,28 @@ public class EmployeeService {
   private final EmployeeRepository repository;
   private Disposable job;
 
-  @PostConstruct
-  public void startJob() {
-    job = Flux.interval(Duration.ofSeconds(30)).flatMap(interval -> statistics()).subscribe();
-  }
-
-  @PreDestroy
-  public void stopJob() {
-    if (!job.isDisposed()) {
-      job.dispose();
-    }
-  }
-
   public Mono<Employee> save(Mono<Employee> employee) {
-    return employee
-        .flatMap(repository::save)
-        .doOnSuccess(
-            savedEmployee ->
-                log.debug(
-                    "Employee {} of boss {} saved.",
-                    savedEmployee.getId(),
-                    savedEmployee.getBossId()));
+    // TODO
+    return Mono.empty();
   }
 
   public Flux<Employee> findAll() {
-    return repository.findAll();
-  }
-
-  public Flux<Employee> findDirectsByBoss(Integer id) {
-    return repository.findByBossId(id);
+    // TODO
+    return Flux.empty();
   }
 
   public Flux<Employee> findAllByBoss(Integer id) {
-    return repository
-        .findById(id)
-        .expand(employee -> repository.findByBossId(employee.getId()))
-        .filter(employee -> !employee.getId().equals(id))
-        .doOnNext(employee -> log.trace("Employee {} found for boss {}", employee.getName(), id));
-  }
-
-  public Mono<Integer> costByBoss(Integer id) {
-    return findAllByBoss(id)
-        .map(Employee::getSalary)
-        .reduce(0, Integer::sum)
-        .doOnNext(cost -> log.trace("The total cost for boss {} is {}", id, cost));
+    // TODO
+    return Flux.empty();
   }
 
   public Flux<String> export() {
-    return findAll()
-        .buffer(2)
-        .flatMap(
-            employees ->
-                Mono.fromCallable(() -> FileExporter.execute(employees))
-                    .publishOn(Schedulers.elastic()))
-        .doOnNext(absolutePath -> log.info("Employees exported to file {}", absolutePath));
+    // TODO
+    return Flux.empty();
   }
 
-  public Mono<Tuple2<Integer, Employee>> statistics() {
-    final Flux<Employee> roots = repository.findByBossIdIsNull();
-
-    final Mono<Integer> totalCost =
-        roots.map(Employee::getId).flatMap(this::costByBoss).reduce(0, Integer::sum);
-
-    final Mono<Employee> employeeWithHighestSalary =
-        roots
-            .map(Employee::getId)
-            .flatMap(this::findAllByBoss)
-            .sort(Comparator.comparingInt(Employee::getSalary).reversed())
-            .take(1)
-            .singleOrEmpty()
-            .cache();
-
-    return totalCost
-        .zipWith(employeeWithHighestSalary)
-        .doOnSuccess(
-            tuple -> {
-              final Integer cost = tuple.getT1();
-              final Employee expensive = tuple.getT2();
-              log.debug(
-                  "The total cost of employees is {}. The most expensive employer is {} ({})",
-                  cost,
-                  expensive.getName(),
-                  expensive.getSalary());
-            });
+  public Mono<Integer> costByBoss(Integer id) {
+    // TODO
+    return Mono.empty();
   }
 }
